2e695bd1f628b88c47013f9d647a3cc2bcfe3c3c
ddq2201 <datdongquoc220102@gmail.com>
2024-09-30 09:50:39 +0700

    first commit

---
+++ demo/src/main/java/com/example/demo/ProductController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping
+    public ResponseEntity<List<String>> getProducts() {
+        return ResponseEntity.ok(Arrays.asList("Product 1", "Product 2", "Product 3"));
+    }
+}
